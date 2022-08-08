1114ee79da9c0bd80aef3775b498ad95b93a42a5
Mohammad-Fawaz/Mohammad-Fawaz <fawaz_farhat@hotmail.com>
2022-08-08 17:03:36 +0500

    First Commit

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,9 @@
-FROM adoptopenjdk/maven-openjdk11 as builder
-WORKDIR /app
-COPY . /app 
-RUN mvn clean install 
-
-FROM adoptopenjdk:11-jre-hotspot
-WORKDIR /app
-ARG JAR_FILE=/app/target/*.jar
-COPY --from=builder $JAR_FILE /app/app.jar
-
-EXPOSE 8080
-CMD ["java", "-jar", "/app/app.jar"]
+FROM public.ecr.aws/j4z9l4i9/maven:3.6.0-jdk-11 AS MAVEN_BUILD
+COPY pom.xml build/pom.xml
+COPY settings.xml /build/settings.xml
+COPY src /build/src/
+WORKDIR /build
+RUN mvn package install -DskipTests -s ./settings.xml
+FROM public.ecr.aws/j4z9l4i9/openjdk:11
+COPY --from=MAVEN_BUILD /build/target/*.jar /app.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
--- infra/aws/eu-west-1/production/variables.tf
+++ infra/aws/eu-west-1/production/variables.tf
@@ -18,14 +18,9 @@ variable "service_name_short" {
   default = "cargarage"
 }
 
-variable "ecs_container_instance_type" {
-  type    = string
-  default = "t2.micro"
-}
-
 variable "codebuild_security_group_name" {
   type    = string
-  default = "vpc-codebuild" 
+  default = "vpc-codebuild"
 }
 
 variable "container_port" {
@@ -38,34 +33,14 @@ variable "host_port" {
   default = "0"
 }
 
-variable "container_desired_count" {
-  type    = string
-  default = "1"
-}
-
-variable "container_reserved_task_memory" {
-  type    = string
-  default = "128"
-}
-
-variable "ecs_cluster_name" {
-  type    = string
-  default = "ecs-cluster"
-}
-
-variable "ecs_tg_healthcheck_endpoint" {
-  type    = string
-  default = "/status"
-}
-
 variable "github_username" {
   type    = string
-  default = "ruanbekker"
+  default = "ShahidSalim-oss"
 }
 
 variable "github_repo_name" {
   type    = string
-  default = "aws-terraform-cicd-java-springboot"
+  default = "CiCDwith-terraform"
 }
 
 variable "github_branch" {
@@ -75,21 +50,7 @@ variable "github_branch" {
 
 variable "github_token" {
   type    = string
-}
-
-variable "route53_hosted_zone" {
-  type    = string
-  default = "example.com" 
-}
-
-variable "route53_record_set" {
-  type    = string
-  default = "www" 
-}
-
-variable "service_hostname" {
-  type    = string
-  default = "www.example.com"
+  default = "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"
 }
 
 variable "codestar_connection_id" {
@@ -98,48 +59,23 @@ variable "codestar_connection_id" {
 }
 
 variable "codebuild_docker_image" {
-  type    = string 
-  default = "aws/codebuild/standard:5.0"
+  type    = string
+  default = "aws/codebuild/standard:4.0"
 }
 
 variable "codepipeline_source_stage_name" {
-  type    = string 
+  type    = string
   default = "Source"
 }
 
 variable "codepipeline_build_stage_name" {
-  type    = string 
+  type    = string
   default = "Build"
 }
 
 variable "codepipeline_deploy_stage_name" {
-  type    = string 
+  type    = string
   default = "Prod"
-}
-
-variable "rds_admin_username" {
-  type    = string
-  default = "admin"
-}
-
-variable "rds_subnet_group_name" {
-  type    = string
-  default = "private"
-}
-
-variable "rds_instance_type" {
-  type    = string
-  default = "db.t2.micro" 
-}
-
-variable "platform_type" {
-  type       = string
-  default = "ecs" 
-}
-
-variable "ssh_keypair_name" {
-  type    = string
-  default = ""
 }
 
 variable "vpc_name" {
